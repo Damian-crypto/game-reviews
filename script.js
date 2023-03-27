fda6915d25136369cf282ab87ac349aa03858c5d
damian chamel <www.damianchamel@gmail.com>
2023-03-27 23:58:44 +0530

    add some features

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function onLoad() {
     // var element = document.querySelectorAll('.yvideo');
     // element.style.width = "900";
 }
+
+async function getFakePerson() {
+    const { results } = await fetch('https://randomuser.me/api').then((res) => res.json());
+
+    var ret = {
+        'name': results[0].name.first + " " + results[0].name.last,
+        'picture': results[0].picture.large
+    };
+
+    return ret;
+}
