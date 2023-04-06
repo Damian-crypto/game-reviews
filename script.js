a1d32f2b32f83fb779f616f741d81fa286bff672
damian chamel <www.damianchamel@gmail.com>
2023-04-06 08:17:17 +0530

    made some changes

--- script.js
+++ script.js
@@ -2,14 +2,3 @@ function onLoad() {
     // var element = document.querySelectorAll('.yvideo');
     // element.style.width = "900";
 }
-
-async function getFakePerson() {
-    const { results } = await fetch('https://randomuser.me/api').then((res) => res.json());
-
-    var ret = {
-        'name': results[0].name.first + " " + results[0].name.last,
-        'picture': results[0].picture.large
-    };
-
-    return ret;
-}
